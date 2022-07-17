f1b6d0e57ad7605f8ef32cfb6e7159d069c6f9a7
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2022-07-17 12:17:59 -0400

    add movies to seed to movie table

---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Movie;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Movie::insert([
+            [
+                'name' => 'Resident Evil',
+                'release_year' => '2002',
+                'rating' => 'R'
+            ],
+            [
+                'name' => 'Resident Evil Apocalypse',
+                'release_year' => '2004',
+                'rating' => 'R'
+            ],
+            [
+                'name' => 'Resident Evil Extinction',
+                'release_year' => '2007',
+                'rating' => 'R'
+            ],
+            [
+                'name' => 'Resident Evil Afterlife',
+                'release_year' => '2010',
+                'rating' => 'R'
+            ],
+            [
+                'name' => 'Resident Evil Retribution',
+                'release_year' => '2012',
+                'rating' => 'R'
+            ],
+            [
+                'name' => 'Resident Evil The Final Chapter',
+                'release_year' => '2016',
+                'rating' => 'R'
+            ],
+        ]);
+    }
+}

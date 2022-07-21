1ca7fc6e12f57953d26eaa98eae6e460f3bdba42
Demetrius Lewis <Demetriuslewis2012@gmail.com>
2022-07-20 21:20:14 -0400

    chore: refactor MovieController to json responses, basic exception handling

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -5,53 +5,62 @@ namespace App\Http\Controllers;
 use App\Models\Movie;
 use App\Http\Requests\StoreMovieRequest;
 use App\Http\Requests\UpdateMovieRequest;
+use Exception;
 use Illuminate\Http\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 
 class MovieController extends Controller
 {
     /**
      * Display a listing of the movies.
      *
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function index()
+    public function index(): JsonResponse
     {
-        return [
-            'data' => Movie::all()
-        ];
+        try {
+            return response()->json([
+                'data' => Movie::all()
+            ]);
+        } catch (Exception $e) {
+            return response()->json('Something went wrong', 500);
+        }
+
     }
 
     /**
      * Display the specified movie.
      *
      * @param  \App\Models\Movie  $movie
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function show(Movie $movie)
+    public function show(Movie $movie): JsonResponse
     {
-        return [
-            'data' => $movie
-        ];
+        try {
+            return response()->json([
+                'data' => $movie
+            ]);
+        } catch(Exception $e) {
+            return response()->json('Something went wrong', 500);
+        }
     }
 
     /**
      * Store a newly created movie in storage.
      *
      * @param  \App\Http\Requests\StoreMovieRequest  $request
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function store(StoreMovieRequest $request)
+    public function store(StoreMovieRequest $request): JsonResponse
     {
-        Movie::create([
-            'name' => $request->name,
-            'release_year' => $request->release_year,
-            'rating' => $request->rating
-        ]);
+        try {
+            $movie = Movie::create($request->validated());
 
-        return [
-            'ok' => true
-        ];
+            return response()->json([
+                'data' => $movie
+            ], 201);
+        } catch(Exception $e) {
+            return response()->json('Something went wrong!', 500);
+        }
     }
 
     /**
@@ -59,33 +68,41 @@ class MovieController extends Controller
      *
      * @param  \App\Http\Requests\UpdateMovieRequest  $request
      * @param  \App\Models\Movie  $movie
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function update(UpdateMovieRequest $request, Movie $movie)
+    public function update(UpdateMovieRequest $request, Movie $movie): JsonResponse
     {
-        $movie->update([
-            'name' => $request->name ?? $movie->name,
-            'release_year' => $request->release_year ?? $movie->release_year,
-            'rating' => $request->rating ?? $movie->rating
-        ]);
+        try {
+            $movie->update([
+                'name' => $request->name ?? $movie->name,
+                'release_year' => $request->release_year ?? $movie->release_year,
+                'rating' => $request->rating ?? $movie->rating
+            ]);
 
-        return [
-            'ok' => true
-        ];
+            $movie->save();
+
+            return response()->json([
+                'data' => $movie
+            ]);
+        } catch(Exception $e) {
+            return response()->json('Something went wrong', 500);
+        }
+
     }
 
     /**
      * Remove the specified movie from storage.
      *
      * @param  \App\Models\Movie  $movie
-     * @return array
+     * @return \Illuminate\Http\JsonResponse
      */
-    public function destroy(Movie $movie)
+    public function destroy(Movie $movie): JsonResponse
     {
-        $movie->delete();
-
-        return [
-            'ok' => true
-        ];
+        try {
+            $movie->delete();
+            return response()->json('Deleted successfully');
+        } catch(Exception $e) {
+            return response()->json('Something went wrong', 500);
+        }
     }
 }
